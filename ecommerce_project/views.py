709581f2612aeb465453e764d729700e7df1b813
arjunbabu9656 <arjunb7025@gmail.com>
2026-03-09 13:20:00 +0530

    Add horizontal category filter bar to homepage and store

--- ecommerce_project/views.py
+++ ecommerce_project/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from store.models import Product
+from store.models import Product, Category
 
 def home(request):
     products = Product.objects.all().filter(is_available=True).order_by('-created_date')[:8]
+    categories = Category.objects.all()
     context = {
         'products': products,
+        'categories': categories,
     }
     return render(request, 'store/index.html', context)
